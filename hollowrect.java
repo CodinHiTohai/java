69205694ac2bd0434cd4130edbbb61ded326bc16
GovindKumar <govindkumar52936@gmail.com>
2026-01-06 20:41:55 +0530

    hollowrectanglepatter

---
+++ hollowrect.java
@@ -0,0 +1,19 @@
+public class hollowrect {
+    
+    public static void main(String[] args) {
+        int n=4;
+        int m=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0||i==3||j==0||j==4){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
